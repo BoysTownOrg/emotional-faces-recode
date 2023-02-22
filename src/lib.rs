1a383f0a23baaf9dffc9363bed84fc655e036119
Seth Bashford <seth.bashford@boystown.org>
2023-02-22 14:54:41 -0600

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,63 @@ fn parse_events(input: &str) -> Vec<Event> {
         .collect::<Vec<Event>>()
 }
 
+#[derive(Debug, PartialEq)]
+enum Condition {
+    Angry,
+    Happy,
+    Neutral,
+}
+
+#[derive(Debug, PartialEq)]
+enum Sex {
+    Male,
+    Female,
+}
+
+#[derive(Debug, PartialEq)]
+struct Trial {
+    correct_response: bool,
+    condition: Condition,
+    sex: Sex,
+    response_time_milliseconds: Option<i64>,
+}
+
+fn reconstruct_trials(events: Vec<Event>) -> Vec<Trial> {
+    let mut trials = Vec::new();
+    let mut i = 0;
+    while i < events.len() - 2 {
+        match events[i].trigger_code {
+            21 => {}
+            22 => {
+                let correct = events[i + 2].trigger_code == 512;
+                let response_time_milliseconds = if correct {
+                    Some(events[i + 2].time_microseconds - events[i + 1].time_microseconds)
+                } else {
+                    None
+                };
+                trials.push(Trial {
+                    correct_response: correct,
+                    condition: Condition::Happy,
+                    sex: Sex::Female,
+                    response_time_milliseconds,
+                });
+                i += 1;
+            }
+            23 => {}
+            31 => {}
+            32 => {}
+            33 => {}
+            _ => {}
+        }
+        i += 1;
+    }
+
+    trials
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::Event;
+    use crate::{Condition, Event, Sex, Trial};
 
     #[test]
     fn parse_events() {
@@ -47,6 +101,33 @@ mod tests {
                 }
             ],
             events
+        );
+    }
+
+    #[test]
+    fn reconstruct_trials() {
+        let trials = crate::reconstruct_trials(vec![
+            Event {
+                time_microseconds: 6293000,
+                trigger_code: 22,
+            },
+            Event {
+                time_microseconds: 6302000,
+                trigger_code: 4118,
+            },
+            Event {
+                time_microseconds: 7288000,
+                trigger_code: 512,
+            },
+        ]);
+        assert_eq!(
+            vec![Trial {
+                correct_response: true,
+                condition: Condition::Happy,
+                sex: Sex::Female,
+                response_time_milliseconds: Some(7288000 - 6302000)
+            }],
+            trials
         );
     }
 }
